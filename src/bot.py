f1140a0cca2c74c2d252bd971e24b5e6b019d424
Bruno Paes <brunopaes05@gmail.com>
2019-08-16 22:25:15 -0300

    Adding send audio/image methods

--- src/bot.py
+++ src/bot.py
@@ -5,7 +5,7 @@ import os
 
 
 path = \
-    os.path.abspath('{}/data/credentials.json'.
+    os.path.abspath('{}/data/settings.json'.
                     format(os.getcwd() + os.sep + os.pardir))
 
 API_TOKEN = json.loads(open(path, 'r').read())['API_TOKEN']
@@ -37,7 +37,39 @@ def echo_message(message):
         The message object.
 
     """
-    bot.reply_to(message, 'Vai se fuder')
+    bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(commands=['help'])
+def send_audio(message):
+    """This function gets the incoming message and sends an vitas audio.
+
+    Parameters
+    ----------
+    message : telebot.types.Message
+        The message object.
+
+    """
+    audio = \
+        open('/Users/brunopaes/Desktop/labs-chickenbot/data/vitsa.mp3', 'rb')
+
+    bot.send_audio(message.chat.id, audio)
+
+
+@bot.message_handler(commands=['image'])
+def send_image(message):
+    """This function gets the incoming message and sends an image.
+
+    Parameters
+    ----------
+    message : telebot.types.Message
+        The message object.
+
+    """
+    img = \
+        open('/Users/brunopaes/Desktop/labs-chickenbot/data/4377539_0.jpg', 'rb')
+
+    bot.send_photo(message.chat.id, img)
 
 
 bot.polling()
